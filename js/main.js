41d1bca2b8f7dd3f30adb23918c092786be842a2
Ulzahk <ulzahk@gmail.com>
2020-07-02 15:05:18 -0500

    Estructura basica del proyecto

---
+++ js/main.js
@@ -0,0 +1,10 @@
+  
+const menuBar = document.getElementById('menu-bar');
+const bg = document.querySelector('.bg');
+const playlists = document.getElementById('playlists');
+const visible = function(){
+    playlists.classList.toggle('visible');
+    bg.classList.toggle('visible');
+};
+menuBar.addEventListener('click', visible);
+bg.addEventListener('click', visible);
